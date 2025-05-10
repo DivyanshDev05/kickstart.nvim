f5d6674928a80a99a794428790a90479ec36daff
office-lap <d.bhatnagar1999@gmail.com>
2025-05-10 15:44:54 +0530

    fix undotree plugin github username

--- init.lua
+++ init.lua
@@ -242,7 +242,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'mbbil/undotree',
+  'mbbill/undotree',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
